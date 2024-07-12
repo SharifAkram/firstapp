c139871e540d6e297998e7b38b00442f88fb6788
Sharif Akram <rifkit@gmail.com>
2024-07-12 18:06:05 -0400

    first commit on first app

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:firstapp/gradient_container.dart';
+
+// Hot reload works effectively with the main function written in this shorthand form:
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    );
+  }
+}
+
+// To use hot reload, function to call runApp function with MyApp
+// as an argument
+/*
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello Miami, v2!!'),
+        ),
+      ),
+    );
+  }
+}
+*/
