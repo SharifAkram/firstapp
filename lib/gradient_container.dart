eedb9545ae87f3b712ffc2b02c48447b1cfc350c
Sharif Akram <rifkit@gmail.com>
2024-07-19 18:46:42 -0400

    create resuable widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,26 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:firstapp/styled_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(child: StyledText('Hello Florida!')),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,10 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 166, 159, 179),
+        ),
       ),
     );
   }
